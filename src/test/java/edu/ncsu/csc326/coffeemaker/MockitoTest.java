d96c6fc1ac91ec6cc4a213c232a44f4468d6ec23
Ing140943 <skladeeing@gmail.com>
2021-09-10 22:46:53 +0700

    verify correctly call test stub and add mock inventory test

--- src/test/java/edu/ncsu/csc326/coffeemaker/MockitoTest.java
+++ src/test/java/edu/ncsu/csc326/coffeemaker/MockitoTest.java
@@ -32,11 +32,12 @@ public class MockitoTest {
     public void testTheUserPayWithEnoughMoney() {
         when(recipe.getPrice()).thenReturn(100);
         when(coffeeMakerMock.getRecipes()).thenReturn(recipes);
-        assertEquals(50, coffeeMakerMock.makeCoffee(0, 50));
-        assertEquals(0, coffeeMakerMock.makeCoffee(0, 100));
-        assertEquals(900, coffeeMakerMock.makeCoffee(0, 1000));
-        assertEquals(100, coffeeMakerMock.makeCoffee(0, 200));
-        assertEquals(25, coffeeMakerMock.makeCoffee(0, 125));
+        assertEquals(50, coffeeMakerMock.makeCoffee(0, 50)); // call getRecipes 2 times
+        assertEquals(0, coffeeMakerMock.makeCoffee(0, 100)); // call getRecipes 4 times
+        assertEquals(900, coffeeMakerMock.makeCoffee(0, 1000)); // call getRecipes 4 times
+        assertEquals(100, coffeeMakerMock.makeCoffee(0, 200)); // call getRecipes 4 times
+        assertEquals(25, coffeeMakerMock.makeCoffee(0, 125)); // call getRecipes 4 times
+        verify(dummyRecipeBook, times(18)).getRecipes();
     }
 
     /**
@@ -51,11 +52,12 @@ public class MockitoTest {
         when(recipe2.getPrice()).thenReturn(50);
         when(recipe3.getPrice()).thenReturn(10);
         when(coffeeMakerMock.getRecipes()).thenReturn(recipes);
-        assertEquals(0, coffeeMakerMock.makeCoffee(1, 50));
-        assertEquals(0, coffeeMakerMock.makeCoffee(0, 100));
-        assertEquals(990, coffeeMakerMock.makeCoffee(2, 1000));
-        assertEquals(100, coffeeMakerMock.makeCoffee(1, 150));
-        assertEquals(0, coffeeMakerMock.makeCoffee(2, 10));
+        assertEquals(0, coffeeMakerMock.makeCoffee(1, 50));  // call getRecipes 4 times
+        assertEquals(0, coffeeMakerMock.makeCoffee(0, 100)); // call getRecipes 4 times
+        assertEquals(990, coffeeMakerMock.makeCoffee(2, 1000)); // call getRecipes 4 times
+        assertEquals(100, coffeeMakerMock.makeCoffee(1, 150)); // call getRecipes 4 times
+        assertEquals(0, coffeeMakerMock.makeCoffee(2, 10));  // call getRecipes 4 times
+        verify(dummyRecipeBook, times(20)).getRecipes();
     }
 
     /**
@@ -65,9 +67,10 @@ public class MockitoTest {
     public void testTheUserPayWithNotEnoughMoney() {
         when(recipe.getPrice()).thenReturn(1000);
         when(coffeeMakerMock.getRecipes()).thenReturn(recipes);
-        assertEquals(50, coffeeMakerMock.makeCoffee(0, 50));
-        assertEquals(100, coffeeMakerMock.makeCoffee(0, 100));
-        assertEquals(300, coffeeMakerMock.makeCoffee(0, 300));
+        assertEquals(50, coffeeMakerMock.makeCoffee(0, 50)); // call getRecipes 2 times
+        assertEquals(100, coffeeMakerMock.makeCoffee(0, 100)); // call getRecipes 2 times
+        assertEquals(300, coffeeMakerMock.makeCoffee(0, 300)); // call getRecipes 2 times
+        verify(dummyRecipeBook, times(6)).getRecipes();
     }
 
     /**
@@ -82,9 +85,10 @@ public class MockitoTest {
         when(recipe2.getPrice()).thenReturn(5000);
         when(recipe3.getPrice()).thenReturn(1000);
         when(coffeeMakerMock.getRecipes()).thenReturn(recipes);
-        assertEquals(50, coffeeMakerMock.makeCoffee(1, 50));
-        assertEquals(100, coffeeMakerMock.makeCoffee(0, 100));
-        assertEquals(600, coffeeMakerMock.makeCoffee(2, 600));
+        assertEquals(50, coffeeMakerMock.makeCoffee(1, 50)); // call getRecipes 2 times
+        assertEquals(100, coffeeMakerMock.makeCoffee(0, 100)); // call getRecipes 2 times
+        assertEquals(600, coffeeMakerMock.makeCoffee(2, 600)); // call getRecipes 2 times
+        verify(dummyRecipeBook, times(6)).getRecipes();
     }
 
     /**
@@ -97,9 +101,10 @@ public class MockitoTest {
         when(recipe.getAmtSugar()).thenReturn(5);
         when(recipe.getAmtMilk()).thenReturn(3);
         when(recipe.getPrice()).thenReturn(200);
-        assertEquals(0, coffeeMakerMock.makeCoffee(0,200));
-        assertEquals(100, coffeeMakerMock.makeCoffee(0,300));
-        assertEquals(40, coffeeMakerMock.makeCoffee(0,240));
+        assertEquals(0, coffeeMakerMock.makeCoffee(0,200)); // call getRecipes 4 times
+        assertEquals(100, coffeeMakerMock.makeCoffee(0,300)); // call getRecipes 4 times
+        assertEquals(40, coffeeMakerMock.makeCoffee(0,240)); // call getRecipes 4 times
+        verify(dummyRecipeBook, times(12)).getRecipes();
     }
 
     /**
@@ -109,9 +114,10 @@ public class MockitoTest {
     public void testNullRecipe() {
         Recipe [] r = new Recipe[] {null};
         when(dummyRecipeBook.getRecipes()).thenReturn(r);
-        assertEquals(20, coffeeMakerMock.makeCoffee(0,20));
-        assertEquals(100, coffeeMakerMock.makeCoffee(0,100));
-        assertEquals(50, coffeeMakerMock.makeCoffee(0,50));
+        assertEquals(20, coffeeMakerMock.makeCoffee(0,20)); // call getRecipes 1 times
+        assertEquals(100, coffeeMakerMock.makeCoffee(0,100)); // call getRecipes 1 times
+        assertEquals(50, coffeeMakerMock.makeCoffee(0,50)); // call getRecipes 1 times
+        verify(dummyRecipeBook, times(3)).getRecipes();
     }
     /**
      * Test the coffeemaker that have enough ingredients for the user order.
@@ -123,12 +129,60 @@ public class MockitoTest {
         when(recipe.getAmtSugar()).thenReturn(3);
         when(recipe.getAmtMilk()).thenReturn(7);
         when(recipe.getPrice()).thenReturn(200);
-        assertEquals(0, coffeeMakerMock.makeCoffee(0,200));
-        assertEquals(100, coffeeMakerMock.makeCoffee(0,300));
-        assertEquals(240, coffeeMakerMock.makeCoffee(0,240));
-        assertEquals(200, coffeeMakerMock.makeCoffee(0,200));
+        assertEquals(0, coffeeMakerMock.makeCoffee(0,200)); // call getRecipes 4 times
+        assertEquals(100, coffeeMakerMock.makeCoffee(0,300)); // call getRecipes 4 times
+        assertEquals(240, coffeeMakerMock.makeCoffee(0,240)); // call getRecipes 3 times
+        assertEquals(200, coffeeMakerMock.makeCoffee(0,200)); // call getRecipes 3 times
+        verify(dummyRecipeBook, times(14)).getRecipes();
     }
 
+    /**
+     * Test that the amount sugar subtract the use of ingredients correctly.
+     */
+    @Test
+    public void testCalculateAmtSugar() {
+        when(coffeeMakerMock.getRecipes()).thenReturn(recipes);
+        when(recipe.getPrice()).thenReturn(0);
+        when(recipe.getAmtSugar()).thenReturn(10);
+        coffeeMakerMock.makeCoffee(0,0);
+        assertEquals("Coffee: 15\nMilk: 15\nSugar: 5\nChocolate: 15\n", coffeeMakerMock.checkInventory());
+    }
+
+    /**
+     * Test that the amount milk subtract the use of ingredients correctly.
+     */
+    @Test
+    public void testCalculateAmtMilk() {
+        when(coffeeMakerMock.getRecipes()).thenReturn(recipes);
+        when(recipe.getPrice()).thenReturn(0);
+        when(recipe.getAmtMilk()).thenReturn(10);
+        coffeeMakerMock.makeCoffee(0,0);
+        assertEquals("Coffee: 15\nMilk: 5\nSugar: 15\nChocolate: 15\n", coffeeMakerMock.checkInventory());
+    }
+
+    /**
+     * Test that the amount coffee subtract the use of ingredients correctly.
+     */
+    @Test
+    public void testCalculateAmtCoffee() {
+        when(coffeeMakerMock.getRecipes()).thenReturn(recipes);
+        when(recipe.getPrice()).thenReturn(0);
+        when(recipe.getAmtCoffee()).thenReturn(10);
+        coffeeMakerMock.makeCoffee(0,0);
+        assertEquals("Coffee: 5\nMilk: 15\nSugar: 15\nChocolate: 15\n", coffeeMakerMock.checkInventory());
+    }
+
+    /**
+     * Test that the amount chocolate subtract the use of ingredients correctly.
+     */
+    @Test
+    public void testCalculateAmtChocolate() {
+        when(coffeeMakerMock.getRecipes()).thenReturn(recipes);
+        when(recipe.getPrice()).thenReturn(0);
+        when(recipe.getAmtChocolate()).thenReturn(10);
+        coffeeMakerMock.makeCoffee(0,0);
+        assertEquals("Coffee: 15\nMilk: 15\nSugar: 15\nChocolate: 5\n", coffeeMakerMock.checkInventory());
+    }
 
     /**
      * Verify that the method has called correctly.
